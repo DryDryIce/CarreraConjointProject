d4a1814b2fea0ba9fa5bb7aef097d91d87bf8be5
DryDryIce <167832910+DryDryIce@users.noreply.github.com>
2026-04-27 21:59:51 -0600

    Primera parte

---
+++ BD.pl
@@ -0,0 +1,49 @@
+% =========================
+% BASE DE DATOS - PROFESIONES
+% =========================
+
+% profesion(Nombre, Afinidades, Antagonias)
+
+profesion(ingenieria_computadores,
+    [matematicas, tecnologia, resolver_problemas, logica],
+    [personas, comunicacion]).
+
+profesion(psicologia,
+    [personas, escuchar, ayudar, empatia],
+    [matematicas, tecnologia]).
+
+profesion(medicina,
+    [personas, ayudar, biologia, responsabilidad],
+    [sangre, tecnologia]).
+
+profesion(derecho,
+    [argumentar, leer, justicia, debate],
+    [matematicas, tecnologia]).
+
+profesion(arquitectura,
+    [diseno, creatividad, matematicas, construccion],
+    [biologia, rutina]).
+
+profesion(ingenieria_industrial,
+    [optimizacion, procesos, matematicas, organizacion],
+    [arte, improvisacion]).
+
+profesion(educacion,
+    [ensenar, personas, comunicacion, paciencia],
+    [tecnologia, numeros]).
+
+profesion(diseno_grafico,
+    [creatividad, arte, diseno, tecnologia],
+    [matematicas, rutina]).
+
+profesion(contabilidad,
+    [numeros, organizacion, analisis, detalle],
+    [creatividad, improvisacion]).
+
+profesion(administracion,
+    [liderazgo, organizacion, negocios, personas],
+    [tecnologia, matematicas]).
+
+profesion(periodismo,
+    [comunicacion, escribir, investigar, personas],
+    [matematicas, rutina]).
